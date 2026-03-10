38c77eef75976d56b8d330f94180554de4717412
Arijit Saha <142727976+Arijitcoding-2005@users.noreply.github.com>
2026-03-10 10:20:01 +0530

    removed frontend from backend repo

--- student-complaint-frontend/src/pages/student/SubmitComplaint.jsx
+++
@@ -1,6 +0,0 @@
-const SubmitComplaint = () => (
-<h2>Submit Complaint</h2>
-);
-
-
-export default SubmitComplaint;
--- student-complaint-frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import api from "../api/axios";
-
-export const studentLogin = (data) => {
-  return api.post("/auth/student/login", data);
-};
-
-export const adminLogin = (data) => {
-  return api.post("/auth/admin/login", data);
-};
