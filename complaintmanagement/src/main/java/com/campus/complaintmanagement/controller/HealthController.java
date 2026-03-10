592141eec2d7a223bfd55d50a6b4db5fabaa9487
Arijit Saha <142727976+Arijitcoding-2005@users.noreply.github.com>
2026-03-10 18:07:05 +0530

    Health Status fixed

--- complaintmanagement/src/main/java/com/campus/complaintmanagement/controller/HealthController.java
+++ complaintmanagement/src/main/java/com/campus/complaintmanagement/controller/HealthController.java
@@ -2,9 +2,11 @@ package com.campus.complaintmanagement.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<?> health() {
